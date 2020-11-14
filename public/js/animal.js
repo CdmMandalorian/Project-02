417a1f98e1eded0444e48bcb8a981056a606f2ee
Patrick Tomas <p.tomas14@gmail.com>
2020-11-14 15:54:47 -0800

    playing around with modal

--- public/js/animal.js
+++ public/js/animal.js
@@ -1,15 +1,14 @@
 var searchByArray = [];
 
+var modal = document.getElementById("myModal");
 var submitBtn = document.getElementById("submitBtn");
 var btn1 = document.getElementById("defaultCheck1");
 var btn2 = document.getElementById("defaultCheck2");
 var btn3 = document.getElementById("defaultCheck3");
 var btn4 = document.getElementById("defaultCheck4");
 
+document.onload = function(){
 
-window.onload = function(){
-
-    var modal = document.getElementById("myModal");
     modal.style.display = "block";
     
     var animalClass = document.getElementById("animalType");
@@ -25,7 +24,6 @@ window.onload = function(){
     animalNote.style.display = "none";
     
     btn1.addEventListener("change", function(event) {
-      //var myLowerLetter = document.querySelector('.checkBox1:checked').value;
       boxChecked1 = true;
       console.log(boxChecked1);
     
@@ -62,10 +60,99 @@ window.onload = function(){
     });
     
     submitBtn.addEventListener("click", function(event){
-        for(var i = 0, i < searchByArray.length, i++){
+        for(var i = 0; i < searchByArray.length; i++){
 
-            searchByArray[i].style.display= "block";
+            searchByArray[i].style.display = "block";
 
         }
+        modal.style.display= "none";
     });
 }
+
+$(document).ready(() => {
+    // This file just does a GET request to figure out which user is logged in
+    // and updates the HTML on the page
+    $.get("/api/user_data").then(data => {
+      $(".member-name").text(data.userName);
+      const userName = data.userName;
+      const sendLocBtn = $("#send-location");
+      const recenterBtn = $("#recenter");
+      const createObserverBtn = $(".add-observation");
+          
+      function handleLoginErr(err) {
+        $("#alert .msg").text(err.responseJSON);
+        $("#alert").fadeIn(500);
+      }
+  
+      createObserverBtn.on("submit", event => {
+        event.preventDefault();
+        const animal = $("#animal-type").val();
+        const animalLng = $("#animal-lng").val();
+        const animalLat = $("#animal-lat").val();
+        const animalNote = $("#animal-note").val();
+        // const animalPic = $("#animal-pic").val();
+  
+        $.post("/api/animals", {
+          animal_species: animal,
+          longitude: animalLng,
+          latiitude: animalLat,
+          hostile: true,
+          foundByUser: userName,
+          note: animalNote,
+          // picture: animalPic
+        })
+          .then(() => {
+            window.location.replace("/members");
+          })
+          .catch(handleLoginErr);
+      });
+  
+      sendLocBtn.on("click", event => {
+        event.preventDefault();
+        const location = marker.getLngLat();
+        $("#animal-lat").val(location.lat);
+        $("#animal-lng").val(location.lng);
+      });
+  
+      recenterBtn.on("click", event => {
+        event.preventDefault();
+        const center = map.getCenter();
+        marker.setLngLat(center);
+      });
+  
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoiYmJyaW50bGUiLCJhIjoiY2toY2VzMXVuMDA1YjJ4bnk3a3Myc2xoOSJ9.utPq30o3rq4GihknsRgSFQ";
+      const coordinates = document.getElementById("coordinates");
+      const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-117, 39],
+        zoom: 10
+      });
+  
+      const marker = new mapboxgl.Marker({
+        draggable: true
+      })
+        .setLngLat([-117, 39])
+        .addTo(map);
+  
+      // Add geolocate control to the map.
+      map.addControl(
+        new mapboxgl.GeolocateControl({
+          positionOptions: {
+            enableHighAccuracy: true
+          },
+          trackUserLocation: true
+        })
+      );
+  
+      function onDragEnd() {
+        const lngLat = marker.getLngLat();
+        coordinates.style.display = "block";
+        coordinates.innerHTML =
+          "Longitude: " + lngLat.lng + "<br />Latitude: " + lngLat.lat;
+      }
+  
+      marker.on("dragend", onDragEnd);
+    });
+  });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,15 +16,24 @@ module.exports = function(app) {
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/");
+      res.redirect("/members");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
+
+  app.get("/signup", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+
   app.get("/members", isAuthenticated, (req, res) => {
-    res.render("../views/animal.handlebars");
+    res.render("animal.handlebars");
   });
 
   app.get("/locationSelect", isAuthenticated, (req, res) => {
