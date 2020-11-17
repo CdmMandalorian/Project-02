add8eb6a28c32c7a83b7f477fcc422fc2922a1fa
bbrintle <bbrintle@gmail.com>
2020-11-17 13:45:33 -0800

    updateing styles

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,19 +1,11 @@
-// Requiring path to so we can use relative routes to our HTML files
-const path = require("path");
-
-// Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
-
-
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
     res.render("index.handlebars")
   });
 
   app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
@@ -21,24 +13,11 @@ module.exports = function(app) {
   });
 
   app.get("/signup", (req, res) => {
-    // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
     res.render("signup.handlebars");
   });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-
-  app.get("/signup", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
-
 
   app.post("/members", isAuthenticated, (req, res) => {
     res.render("members.handlebars");
